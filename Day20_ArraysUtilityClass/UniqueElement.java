db4f6d397cf3344e1a9a4f10775ea83560e66ccf
Dilara <dcavus@gmail.com>
2022-06-08 15:47:02 -0700

    week1-week20 files

--- Day20_ArraysUtilityClass/UniqueElement.java
+++ Day20_ArraysUtilityClass/UniqueElement.java
@@ -5,15 +5,15 @@ import utilities.ArrayUtility;
 public class UniqueElement {
     public static void main(String[] args) {
 
-        int[] numbers={13,1,5,6,6,12};
-       /* for (int each : numbers) {
-            int counter=0;
+        int[] numbers = {13, 1, 5, 6, 6, 12};
+        for (int each : numbers) {
+            int counter = 0;
             for (int i : numbers) {
-                if (i==each) counter++;
+                if (i == each) counter++;
             }
-            if (counter==1) System.out.print(each+" ");*/
-        ArrayUtility.findUnique(numbers);
+            if (counter == 1) System.out.print(each + " ");
+
 
         }
     }
-
+}
